83ac9ad8664741f18294a7c4f0f1efa5d1436e3c
avrpic8 <avrpic8@gmail.com>
2021-08-24 16:03:40 +0430

    HasQueryBuilder complete

--- system/Database/Traits/HasQueryBuilder.php
+++ system/Database/Traits/HasQueryBuilder.php
@@ -34,7 +34,7 @@ trait HasQueryBuilder{
     }
 
     protected function setOrderBy($name, $expression){
-        array_push($this->orderBy, $name . ' ' . $expression);
+        array_push($this->orderBy, $this->getAttributeName($name) . ' ' . $expression);
     }
 
     protected function resetOrderBy(){
@@ -67,5 +67,76 @@ trait HasQueryBuilder{
         $this->resetOrderBy();
         $this->resetLimit();
         $this->removeValues();
+    }
+
+    protected function executeQuery(){
+        $query = '';
+        $query .= $this->sql;
+        if(!empty($this->where)){
+            $whereString = '';
+            foreach ($this->where as $where){
+                $whereString == '' ? $whereString .= $where['condition'] : $whereString .= ' ' . $where['operator'] .
+                    ' ' . $where['condition'];
+            }
+            $query .= ' WHERE ' . $whereString;
+        }
+
+        if(!empty($this->orderBy)){
+            $query .= ' ORDER BY ' . implode(', ', $this->orderBy);
+        }
+        if(!empty($this->limit)){
+            $query .= ' LIMIT ' . $this->limit['from'] . ' , ' . $this->limit['number'] .  ' ';
+        }
+        $query .= ' ;';
+        echo $query. '<hr>/';
+
+        $pdoInstance = DBConnection::getDBConnectionInstance();
+        $statement = $pdoInstance->prepare($query);
+
+        if(sizeof($this->bindValues) > sizeof($this->values)){
+            sizeof($this->bindValues) > 0 ? $statement->execute($this->bindValues) : $statement->execute();
+        }
+        else{
+            sizeof($this->values) > 0 ? $statement->execute(array_values($this->values)) : $statement->execute();
+        }
+
+        return $statement;
+    }
+
+    protected function getCount(){
+        $query = '';                // users.*
+        $query .= "SElECT COUNT(".$this->getTableName().".*) FROM ".$this->getTableName();
+
+
+        if(!empty($this->where)){
+            $whereString = '';
+            foreach ($this->where as $where){
+                $whereString == '' ? $whereString .= $where['condition'] : $whereString .= ' ' . $where['operator'] .
+                    ' ' . $where['condition'];
+            }
+            $query .= ' WHERE ' . $whereString;
+        }
+        $query .= ' ;';
+
+        $pdoInstance = DBConnection::getDBConnectionInstance();
+        $statement = $pdoInstance->prepare($query);
+
+        if(sizeof($this->bindValues) > sizeof($this->values)){
+            sizeof($this->bindValues) > 0 ? $statement->execute($this->bindValues) : $statement->execute();
+        }
+        else{
+            sizeof($this->values) > 0 ? $statement->execute(array_values($this->values)) : $statement->execute();
+        }
+
+        return $statement->fetchColumn();
+    }
+
+    protected function getTableName(){
+        return ' `' .$this->table. '`';
+    }
+
+    protected function getAttributeName($attribute){
+        // `users`.`age`
+        return ' `' .$this->table. '`.`' .$attribute . '` ';
     }
 }

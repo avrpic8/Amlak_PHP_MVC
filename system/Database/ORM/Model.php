ae68526b064d7752f5386218a2da3b6261d64590
avrpic8 <avrpic8@gmail.com>
2021-08-25 09:54:07 +0430

    HasAttributes start

--- system/Database/ORM/Model.php
+++ system/Database/ORM/Model.php
@@ -11,4 +11,14 @@ use System\Database\Traits\HasRelation;
 abstract class Model{
 
     use HasCRUD,HasAttributes,HasMethodCaller,HasQueryBuilder,HasRelation;
+
+    protected $table;
+    protected $fillable = [];
+    protected $hidden = [];
+    protected $casts = [];
+    protected $primaryKey = 'id';
+    protected $createdAt = 'created_at';
+    protected $updatedAt = 'updated_at';
+    protected $deletedAt = null;
+    protected $collection = [];
 }
--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -1,5 +1,41 @@
 <?php
 
+namespace System\Database\Traits;
+
 trait HasAttributes{
 
+    private function registerAttribute($object, string $attribute, $value){
+        // $user->name
+        $this->inCastsAttributes($attribute) == true ?
+            $object->$attribute = $this->castDecodeValue($attribute, $value) :
+            $object->$attribute = $value;
+    }
+
+    protected function arrayToAttribute(){
+
+    }
+
+    protected function arrayToObject(){
+
+    }
+
+    private function inHiddenAttributes(){
+
+    }
+
+    private function inCastsAttributes(){
+
+    }
+
+    private function castDecodeValue(){
+
+    }
+
+    private function castEncodeValue(){
+
+    }
+
+    private function arrayToCastEncodeValue(){
+
+    }
 }
